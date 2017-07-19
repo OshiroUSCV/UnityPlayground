1f43da607a5d173074b43c70613795cb2227678d
Brian Valdillez <valdillez@alumni.usc.edu>
2017-07-19 15:25:27 -0700

    WIP: Creating persistent single gamestate setup for storing data between scenes.

---
+++ Assets/Scripts/GamestateManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamestateManager : MonoBehaviour
+{
+
+    // Static instance
+    public static GamestateManager gamestate = null;
+
+    private void Awake()
+    {
+        // Case: Static instance not set; make this the persistent object
+        if (gamestate == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            gamestate = this;
+        }
+        // Case: Static instance already exists, destroy this duplicate
+        else
+        if (gamestate != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
