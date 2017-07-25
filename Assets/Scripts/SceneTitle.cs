6875aebd03559067a8febba7ca22c7e312b1a728
Brian Valdillez <valdillez@alumni.usc.edu>
2017-07-25 11:41:33 -0700

    Messing around on the title screen and some basic scene-loading action.

---
+++ Assets/Scripts/SceneTitle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/**
+ *  CLASS: SceneTitle 
+ *  Script to handle a variety of title screen things?
+ */
+public class SceneTitle : MonoBehaviour
+{
+    private string[] m_pathScenes =
+    {
+        "Scene_Title",
+        "Scene_Gym"
+    };
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Handles clicks in the scene select
+    public void OnSceneSelectClick(int i)
+    {
+        if (i >= 0 && i < m_pathScenes.Length)
+        {
+            SceneManager.LoadScene(m_pathScenes[i], LoadSceneMode.Single);
+        }
+    }
+}
