57db1dea5b75d5a82fe24bc06742358846173982
Brian Valdillez <valdillez@alumni.usc.edu>
2018-02-16 17:33:06 -0800

    Hooked up Black/White/Remove functionality to Left/Right/Middle clicking. Also, removed collider on black and white pieces so raycasting can always hit the board.

--- Assets/Go/Scripts/GoBoard.cs
+++ Assets/Go/Scripts/GoBoard.cs
@@ -17,7 +17,7 @@ public class GoBoard : MonoBehaviour
     protected float txtGridSquareLengthSide;  // Length of each side of a grid-square (in pixels)
     protected float txtGridPosRadiusSq;       // Radius for click detection of each grid position (squared)
 
-    protected Vector2[,] txtGridPositions; 
+    protected GoPoint[,] gridPoints; 
 
     // Use this for initialization
     void Start ()
@@ -28,14 +28,22 @@ public class GoBoard : MonoBehaviour
         txtGridPosRadiusSq = (txtGridSquareLengthSide) * 0.25f;
         txtGridPosRadiusSq *= txtGridPosRadiusSq;
 
-        // Populate the grid positions 2D array 
-        // :NOTE: Later on this array will probably be more substantial and contain more information regarding the board state
-        txtGridPositions = new Vector2[gridSize, gridSize];
+        // Populate the grid points 2D array 
+        gridPoints = new GoPoint[gridSize, gridSize];
         for (int x = 0; x < gridSize; x++)
         {
             for (int y = 0; y < gridSize; y++)
             {
-                txtGridPositions[x, y] = textureCoordsUL + (txtGridSquareLengthSide * new Vector2(x,y));
+                Vector2 point_board = new Vector2(x, y);
+                Vector2 pos_txt     = textureCoordsUL + (txtGridSquareLengthSide * new Vector2(x, y));
+
+                // Convert grid position to board's local position
+                float pos_x = transform.localScale.x * (((point_board.x * txtGridSquareLengthSide) + textureCoordsUL.x) / textureDimensions.x);
+                pos_x -= (0.5f * transform.localScale.x);
+                float pos_z = transform.localScale.z * (((point_board.y * txtGridSquareLengthSide) + textureCoordsUL.y) / textureDimensions.y);
+                pos_z -= (0.5f * transform.localScale.z);
+                Vector3 pos_3D      = new Vector3(pos_x, 0.0f, pos_z);
+                gridPoints[x, y]    = new GoPoint(point_board, pos_txt, pos_3D);
             }
         }
     }
@@ -45,12 +53,44 @@ public class GoBoard : MonoBehaviour
 		
 	}
 
+    private void OnMouseOver()
+    {
+        // Click (Any)
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
+        {
+
+        }
+        // Left: Black Piece
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnMouseClick(true); 
+        }
+        // Right: White Piece
+        else
+        if (Input.GetMouseButtonDown(1))
+        {
+            OnMouseClick(false);
+        }
+        // Middle: Remove
+        else
+        if (Input.GetMouseButtonDown(2))
+        {
+            RemovePiece(GetClosestGridPoint());
+        }
+    }
+
     private void OnMouseDown()
     {
+        
+    }
+
+    private void OnMouseClick(bool bLeft)
+    {
+        // For clarity/readability
+        bool b_black = bLeft;
+
         // Trying w/ Raycast
-        // Convert screen click space into world position
-        //Vector3 pos_mouse = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane)); // <<< PROBLEM HERE
-       // print("Click Pos (World): " + pos_mouse.ToString("F4"));
+        // Cast ray from current mouse position to board
         Ray ray_click = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         RaycastHit hit_info;
@@ -60,38 +100,26 @@ public class GoBoard : MonoBehaviour
             Vector2 pos_grid = GetClosestGridSpace(hit_info.point);
 
             // Create piece on that position :TODO: Verify if within radius
-            CreatePiece(pos_grid, true);
-            //CreatePiece(hit_info.point, true);
+            CreatePiece(pos_grid, b_black);
         }
+    }
 
-        ///////////////////////
-        /** 
-            //TRYING W/ BOUNDS
-        // Retrieve plane associated w/ Go Board
-        Collider plane = gameObject.GetComponent<Collider>();
-        if (plane != null)
+
+    // @return  Closest point on board if mouse currently hovering over board; (-1,-1) otherwise
+    private Vector2 GetClosestGridPoint()
+    {
+        Vector2 point_closest = new Vector2(-1, -1); 
+
+        // Cast ray from current mouse position to board
+        Ray ray_click = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit_info;
+        if (Physics.Raycast(ray_click, out hit_info))//Physics.Raycast(pos_mouse, Vector3.down, out hit_info, /*Maximum Distance */1000.0f))
         {
-            // Convert screen click space into world position
-            Vector3 pos_mouse = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
-
-            // Retrieve position on the board
-            Vector3 pos_board = plane.ClosestPointOnBounds(pos_mouse);
-
-            Vector2 pos_grid = GetClosestGridSpace(pos_board);
-            
-
-        
-            
-
-
-            // DEBUG: Output
-            //print("Click Pos (Screen): " + Input.mousePosition.ToString("F4"));
-            //print("Click Pos (World): " + pos_mouse.ToString("F4"));
-            print("Click Pos (Board) : " +pos_board.ToString("F4"));
-
-            CreatePiece(pos_board, true);
+            // Determine closest grid space
+            point_closest = GetClosestGridSpace(hit_info.point);
         }
-        **/
+
+        return point_closest;
     }
 
     private Vector2 GetClosestGridSpace(Vector3 posWorld)
@@ -114,11 +142,11 @@ public class GoBoard : MonoBehaviour
         // ... and then multiply by texture dimensions
         pos_txt = new Vector2(pos_txt.x * textureDimensions.x, pos_txt.y * textureDimensions.y);
 
-        print("Position Txt: " + pos_txt.ToString("F4"));
+        //print("Position Txt: " + pos_txt.ToString("F4"));
 
         // Next is to identify which grid square we are in, to identify our closest point
         Vector2 grid_square = (pos_txt - textureCoordsUL) / txtGridSquareLengthSide;
-        print("Position Grid: " + grid_square.ToString("F4"));
+        //print("Position Grid: " + grid_square.ToString("F4"));
 
         // Round & Clamp results
         int grid_x = (int)(Mathf.Clamp(Mathf.Round(grid_square.x), 0, gridSize - 1));
@@ -129,21 +157,50 @@ public class GoBoard : MonoBehaviour
         return new Vector2(grid_x, grid_y);
     }
 
-    private void CreatePiece(Vector2 posGrid, bool bIsBlack)
+    private GameObject CreatePiece(Vector2 posGrid, bool bIsBlack)
     {
-        // Convert grid position to board's local position
-        float x = transform.localScale.x * (((posGrid.x * txtGridSquareLengthSide) + textureCoordsUL.x) / textureDimensions.x);
-        x -= (0.5f * transform.localScale.x);
-        float z = transform.localScale.z * (((posGrid.y * txtGridSquareLengthSide) + textureCoordsUL.y) / textureDimensions.y);
-        z -= (0.5f * transform.localScale.z);
-        Vector3 pos_local = new Vector3(x, transform.localPosition.y, z); // Offset Y by half the piece's height, aka scale-Y
+        //// Convert grid position to board's local position
+        //float x = transform.localScale.x * (((posGrid.x * txtGridSquareLengthSide) + textureCoordsUL.x) / textureDimensions.x);
+        //x -= (0.5f * transform.localScale.x);
+        //float z = transform.localScale.z * (((posGrid.y * txtGridSquareLengthSide) + textureCoordsUL.y) / textureDimensions.y);
+        //z -= (0.5f * transform.localScale.z);
+        //Vector3 pos_local = new Vector3(x, transform.localPosition.y, z);
+
+        int point_x = (int)posGrid.x;
+        int point_y = (int)posGrid.y;
+
+        // Check if a piece already exists on given spot. If so, we cannot create a new one
+        if (!gridPoints[point_x, point_y].IsEmpty())
+        {
+            Debug.Log("[GB] Could not create @ ( " + point_x + ", " + point_y + "); already occupied");
+            return null;
+        }
+
         // Spawn
-        CreatePiece(pos_local, bIsBlack);
+        // :TODO:Offset Y by half the piece's height, aka scale-Y
+        Debug.Log("[GB] Piece created @ ( " + point_x + ", " + point_y + ")");
+        GameObject piece_new = CreatePiece(gridPoints[point_x, point_y].PosLocal, bIsBlack);
+        gridPoints[point_x, point_y].SetPiece(piece_new, bIsBlack);
+        return piece_new;
     }
 
-    private void CreatePiece(Vector3 posSpawn, bool bIsBlack)
+    private GameObject CreatePiece(Vector3 posSpawn, bool bIsBlack)
     {
         GameObject piece_new = Instantiate((bIsBlack ? prefabPieceBlack : prefabPieceWhite), posSpawn/*new Vector3(-0.5f, 0.0f, -1.0f)*/, Quaternion.identity, transform);
+        return piece_new;
+    }
 
+    private bool RemovePiece(Vector3 posGrid)
+    {
+        int x = (int)posGrid.x;
+        int y = (int)posGrid.y;
+        return gridPoints[x, y].RemovePiece();
+    }
+
+    private bool RemovePiece(Vector2 posGrid)
+    {
+        int x = (int)posGrid.x;
+        int y = (int)posGrid.y;
+        return gridPoints[x, y].RemovePiece();
     }
 }
---
+++ Assets/Go/Scripts/GoPoint.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Relevant data for a single point on a Go Board
+public class GoPoint
+{
+    public Vector2 PointBoard = new Vector2(-1, -1);    // (X,Y) point on the Go Board
+    public Vector2 PosTexture = new Vector2(-1, -1);  // 2D texture position, used to detect closest clicks
+    public Vector3 PosLocal = new Vector3();       // 3D position of the point in the Go Board's local space
+
+    protected GameObject GoPiece = null;                // GameObject representing the board piece, or null if empty
+
+    //public GoPoint() { }
+
+    public GoPoint(Vector2 pointBoard, Vector2 posTxt, Vector3 pos3D)
+    {
+        PointBoard  = pointBoard;
+        PosTexture  = posTxt;
+        PosLocal    = pos3D;
+    }
+
+    public bool IsEmpty()
+    {
+        return (GoPiece == null);
+    }
+
+    public void SetPiece(GameObject piece, bool bIsBlack)
+    {
+        GoPiece = piece;
+    }
+
+    public bool RemovePiece()
+    {
+        bool b_removed = false;
+        if (!IsEmpty())
+        {
+            Object.Destroy(GoPiece);
+            b_removed = true;
+        }
+        GoPiece = null;
+
+        return b_removed;
+    }
+
+    //public GoColor GetColor()
+    //{
+    //    return GoColor.GC_White; 
+    //}
+
+}
+
+/**
+public class GoPoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+*/
